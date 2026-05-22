6a8b4f23134b4529816b3131c5acc4f2e9e5fb8c
Theodor <1tidde@gmail.com>
2026-05-22 17:33:13 +0200

    CharacterChoiceView created, navigation fixed

---
+++ LevelUpLife/CharacterChoiceView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterChoiceView.swift
+//  LevelUpLife
+//
+//  Created by Theodor Nilsson on 2026-05-22.
+//
+
+import SwiftUI
+
+struct CharacterChoiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterChoiceView()
+}
