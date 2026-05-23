2e7adea4919cfa908737b8693ad0362cc274a3d8
Theodor <1tidde@gmail.com>
2026-05-23 11:41:43 +0200

    ModuleChoiceView added and basic layout for CharacterChoiceView improved.

---
+++ LevelUpLife/ModuleSelectorView.swift
@@ -0,0 +1,18 @@
+//
+//  ModuleSelectorView.swift
+//  LevelUpLife
+//
+//  Created by Theodor Nilsson on 2026-05-23.
+//
+
+import SwiftUI
+
+struct ModuleSelectorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModuleSelectorView()
+}
