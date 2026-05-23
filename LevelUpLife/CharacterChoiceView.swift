f9b68d32eef66b62e8dca48f666090c68f1e696b
Theodor <1tidde@gmail.com>
2026-05-23 11:48:42 +0200

    Update view code

--- LevelUpLife/CharacterChoiceView.swift
+++ LevelUpLife/CharacterChoiceView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct CharacterChoiceView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             HStack(spacing: 10){
-                ForEach(0..<3, id: \.self) { number in
-                    Button(action:{}, label: {Text("Character \(1)")})
+                ForEach(1..<4, id: \.self) { number in
+                    Button(action:{}, label: {Text("Character \(number)")})
                 }
+                
             }
+            .padding(.vertical, 100)
         
+            Text("Stats")
+            HStack() {
+                Text("Health")
+                    .font(.headline)
+                Spacer()
+                Text("Fight power")
+                Spacer()
+                Text("Spell damage")
+            }
+            .padding(.horizontal, 15)
+            HStack(){
+                Text("Stat1")
+                Spacer()
+                Text("Stat2")
+                Spacer()
+                Text("Stat3")
+            }
+            .padding(.horizontal, 15)
+            
+            //Character description
+            Text("This is a character that's cool")
+                .padding(15)
+            
             Spacer()
         }
         .padding(.vertical, 100)
--- LevelUpLife/ModuleSelectorView.swift
+++ LevelUpLife/ModuleSelectorView.swift
@@ -8,10 +8,51 @@
 import SwiftUI
 
 struct ModuleSelectorView: View {
+    
+    let columns = [
+        GridItem(.flexible(), spacing: 10),
+        GridItem(.flexible(), spacing: 10)
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            //MARK: Content
+            VStack{
+                Text("Choose your Adventure")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                Spacer()
+                ScrollView{
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach(0..<8, id:\.self) { number in
+                            Button(action: {},
+                                   label: {
+                                Text("button \(number)")
+                                    .frame(maxWidth: .infinity)
+                                    .frame(height: 50)
+                                    .foregroundStyle(.cyan)
+                                    .padding(15)
+                            }
+                            )
+                            .buttonStyle(.glass)
+                            .buttonBorderShape(.capsule)
+                            .foregroundStyle(.black)
+                            .padding(15)
+                        }
+                    }
+                }
+            }
+        .background {
+            //MARK: Background
+            Image("AdventureBackground")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+        }
     }
 }
+}
+
 
 #Preview {
     ModuleSelectorView()
