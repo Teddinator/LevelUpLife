d7da5b3e402b023812b2519f839d863a6c458df2
Theodor <1tidde@gmail.com>
2026-05-27 17:04:07 +0200

    CharacterChoiceView Improved

---
+++ LevelUpLife/Views/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  LevelUpLife
+//
+//  Created by Theodor Nilsson on 2026-05-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            ZStack{
+                //-MARK: Background
+                LinearGradient(colors: [Color.blue, Color.green], startPoint: .bottom, endPoint: .top)
+                    .ignoresSafeArea()
+                
+                //-MARK: Content
+                VStack(alignment: .leading, spacing: 10){
+                    Text("The app that will take your life to the next level \n Are you ready to level up?")
+                    Text("")
+                    NavigationLink(destination: CharacterChoiceView()){
+                        Text("Get Started")
+                            .foregroundStyle(.black)
+                            .padding(.horizontal, 20)
+                            .padding(.vertical, 10)
+                        
+                    }
+                    .buttonStyle(.bordered)
+                    .buttonBorderShape(.capsule)
+                }
+            }
+            .navigationTitle("LevelUp Adventure")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
